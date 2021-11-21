3d9ba8e7cb7dd505e5ad88f1bf540aee98c29d69
Ubuntu <ubuntu@ip-172-31-27-61.ec2.internal>
2021-11-21 19:44:36 +0000

    fixed style

--- Energy.swift
+++ Energy.swift
@@ -1,31 +1,36 @@
 //
-//  Logs.swift
+//  Energy.swift
 //
 //  Created by Ryan Chung
 //  Created on 2021-11-18
 //  Version 1.0
 //  Copyright (c) 2020 Ryan Chung. All rights reserved.
 //
-//
+//  This program calculates the amount of energy produced by a given mass.
 //
 
 import Foundation
 
-// Variable Declaration
 let lightSpeed: Double = 2.998 * pow(10, 8)
-var mass: Double
-var energy: Double
 
 // User prompt
 print("Enter the mass (kg): ", terminator: "")
 
-// Gathers the user input and converts it to Float
-mass = Double(readLine()!)!
+// Gathers the user input and makes sure it isn't empty
+guard let massString = readLine(), !massString.isEmpty else {
+    print("Something went wrong.")
+    exit(001)
+}
 
-// Calculates the max amount of logs that can fit in the truck
-energy = mass * pow(lightSpeed, 2)
+// Validates and makes sure it is possible to convert to Double.
+if let mass = Double(massString) {
 
-// Outputs the result of the calculation to the user
-print("This mass would create \(energy)J")
+    // Calculates E = mc^2 to find the amount of energy produced.
+    let energy = mass * pow(lightSpeed, 2)
+
+    print("This mass would create \(energy)J")
+} else {
+    print("Something went wrong.")
+}
 
 print("\nDone.")
